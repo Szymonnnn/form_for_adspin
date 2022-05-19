9bbfb02d81d7bd0ae9bf7ac8ccc187d5e0bafebe
japko-7 <filipjablonski@icloud.com>
2022-05-19 12:01:16 +0200

    fix endpoint

--- js/service.js
+++ js/service.js
@@ -1,7 +1,7 @@
 // API_URL = "https://adspin.herokuapp.com/playground/";
 // TOKEN_ENDPOINT = "sessionToken";
 API_URL = "http://localhost:8080/adspin_backend_mock/";
-TOKEN_ENDPOINT = "session_token";
+TOKEN_ENDPOINT = "session_token.php";
 FORM_ENDPOINT = "submit";
 AD_LOAD_ENDPINT = "getNewImages";
 FEEDBACK_ENDPOINT = "liked";
